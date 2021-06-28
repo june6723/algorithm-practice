bd90c80dc370dceb66a02779c17befc82f5ce0c4
june6723 <june6723@naver.com>
2021-06-28 23:41:48 +0900

    Merge sorted array v2

--- writing-python/Array/4-2.merge_sorted.py
+++ writing-python/Array/4-2.merge_sorted.py
@@ -1,1 +1,22 @@
-# git commit --amend
+from typing import List
+
+nums1 = [2,8,10]
+nums2 = [5]
+
+def brute_force(nums1: List[int], nums2: List[int]) -> None:
+  n = len(nums1)
+  m = len(nums2)
+  k1 = 0
+
+  for i in range(0,n):
+    if nums1[k1] > nums2[0]:
+      temp = nums1[k1]
+      nums1[k1] = nums2[0]
+      nums2[0] = temp
+      k1 += 1
+      nums2[:] = sorted(nums2)
+    else:
+      k1 += 1
+
+brute_force(nums1,nums2)
+print(nums1, nums2)
