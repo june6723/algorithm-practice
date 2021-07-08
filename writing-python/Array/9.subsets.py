ef479c92a853af645daed4a7b22cccea69300c21
june6723 <june6723@naver.com>
2021-07-08 23:02:59 +0900

    solution with binary expression

--- writing-python/Array/9.subsets.py
+++ writing-python/Array/9.subsets.py
@@ -13,6 +13,18 @@ def subsets_recursive(nums: List[int],
     subsets_recursive(nums, res, sub, i+1)
     sub.pop()
     
+def subsets_binary(nums: List[int]) -> List[List[int]]:
+  res = []
+  nums_len = len(nums)
+  nth_bit = 1 << nums_len
+
+  for i in range(2 ** nums_len):
+    bitmask = bin(i | nth_bit)[3:]
+
+    res.append(
+      [nums[j] for j in range(nums_len) if bitmask[j] == '1']
+    )
+  return res
 
 
 nums = [1,2,3]
@@ -20,3 +32,4 @@ res = []
 
 subsets_recursive(nums, res, [], 0)
 print(res)
+print(subsets_binary(nums))
