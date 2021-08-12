ecc3f3cb56cc9711a471a0654fd2aa4b77ec304e
june6723 <june6723@naver.com>
2021-08-12 23:17:27 +0900

    cycle detection

---
+++ writing-python/LinkedList/2.cycle_detection.py
@@ -0,0 +1,36 @@
+class Node:
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
+
+def hasCycle(head: Node) -> bool:
+  outer = head
+  node_cnt = 0
+
+  while (outer != None and outer.next != None):
+    outer = outer.next
+    node_cnt += 1
+
+    visit = node_cnt
+    inner = head
+
+    matched = 0
+    while (visit > 0):
+      if outer != inner:
+        inner = inner.next
+      if outer == inner:
+        matched += 1
+        print(inner, outer)
+      if matched == 2:
+        return True
+      visit -= 1
+  return False
+
+node4 = Node(4, None)
+node3 = Node(3, node4)
+node2= Node(2, node3)
+node1 = Node(1, node2)
+node0 = Node(0, node1)
+node4.next = node0
+
+print(hasCycle(node0))
