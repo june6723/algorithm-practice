94aae13f42b4edf150512e7644444cacb505cfb7
june6723 <june6723@naver.com>
2021-08-23 23:30:15 +0900

    recursion basic

--- writing-python/StackAndRecursion/test.py
+++ writing-python/StackAndRecursion/test.py
@@ -1,7 +1,19 @@
-stack = []
+nums = [1,2,3,4,5]
 
-stack.append(1)
-stack.append(2)
-stack.append(3)
+def sum_elements(nums):
+  sum = 0
+  for i in nums:
+    sum = sum + i
+  return sum
 
-stack.pop()
+print(sum_elements(nums))
+
+def sum_recursion(nums):
+  if len(nums) == 0:
+    return 0
+  if len(nums) == 1:
+    return nums[0]
+
+  return nums[0] + sum_recursion(nums[1:])
+
+print(sum_recursion(nums))
