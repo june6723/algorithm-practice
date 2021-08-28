257b150a96d2af1050428793e7f6d80e4c6d64ba
june6723 <june6723@naver.com>
2021-08-28 22:26:02 +0900

    permutation2

--- writing-python/StackAndRecursion/3.permutation.py
+++ writing-python/StackAndRecursion/3.permutation.py
@@ -13,6 +13,35 @@ def find_permutation(s):
 
   return ans
 
+def find_permutation_copy(s):
+  if len(s) == 1:
+    return [s]
+  
+  ans = []
+  curr = s[0]
+  s = s[1:]
+  words = find_permutation_copy(s)
+
+  for sub in words:
+    for i in range(len(sub)+1):
+      ans.append("".join([sub[:i], curr, sub[i:]]))
+  return ans
+
+def permutation_change_pos(s, i):
+  if i + 1 == len(s):
+    return [s]
+  
+  sub = []
+  for pos in range(i, len(s)):
+    if pos == i:
+      sub = sub + permutation_change_pos(s, i+1)
+    else:
+      pos_changed = list(s)
+      pos_changed[pos] = s[i]
+      pos_changed[i] = s[pos]
+      sub = sub + permutation_change_pos("".join(pos_changed), i+1)
+  return sub
+
 s = "abc" 
-res = find_permutation(s)
+res = permutation_change_pos(s, 0)
 print(*res)
