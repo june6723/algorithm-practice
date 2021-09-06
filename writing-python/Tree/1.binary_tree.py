475b3fc38e92df9816f707e6e68b61555ecdb04b
june6723 <june6723@naver.com>
2021-09-06 23:11:49 +0900

    BST delete

--- writing-python/Tree/1.binary_tree.py
+++ writing-python/Tree/1.binary_tree.py
@@ -61,7 +61,57 @@ class BinarySearchTree:
     self._inorder_rec(node.left, result)
     result.append(node.data)
     self._inorder_rec(node.right, result)
+
+  def find_min_node(self, node: Node):
+    while node.left:
+      node = node.left
+    return node
+  
+  def delete(self, data):
+    self._delete_data(self.__root, data)
     
+  def _delete_data(self, node: Node, data):
+    parent = None
+    curr = node
+    # Search 
+    while curr and curr.data != data:
+      parent = curr
+      if curr.data < data:
+        curr = curr.right
+      else:
+        curr = curr.left  
+    if curr == None:
+      return node
+    # 1. Leaf node 일 경우
+    if curr.left == None and curr.right == None:
+      if curr != node:
+        if parent.left == curr:
+          parent.left = None
+        else:
+          parent.right = None
+      else:
+        node = None
+    # 2. 자식 노드가 2개 일 경우
+    elif curr.left and curr.right:
+      min_node_in_right_tree = self.find_min_node(curr.right)
+      min_data = min_node_in_right_tree.data
+      self._delete_data(node, min_data)
+      curr.data = min_data
+    # 3. 자식 노드가 1개 일 경우
+    else:
+      if curr.left:
+        child = curr.left
+        if parent.left == curr:
+          parent.left = child
+        else:
+          parent.right = child
+      else :
+        child = curr.right
+        if parent.left == curr:
+          parent.left = child
+        else:
+          parent.right = child
+    return node
 
 bst = BinarySearchTree()
 bst.insert(20)
@@ -73,4 +123,5 @@ bst.insert(18)
 bst.insert(11)
 bst.insert(21)
 bst.insert(15)
+bst.delete(18)
 print(bst.inorder_traverse())
